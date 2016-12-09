61085f35029c75372d5e12a9734cccd00141743d
Tristan Crockett <tristan.h.crockett@gmail.com>
2016-12-09 17:41:02 -0600

    Implement local mode

--- utils/s3.py
+++ utils/s3.py
@@ -2,6 +2,8 @@
 Common S3 utilities
 """
 import boto
+from config import config
+import logging
 
 
 def split_s3_path(path):
@@ -23,6 +25,9 @@ def upload(s3_conn, filename, s3_path):
         filename: local filename
         s3_path: the destination path on s3
     """
+    if config.get('local_mode', False):
+        logging.info('Local mode is set, forgoing s3 upload')
+        return
     bucket_name, prefix = split_s3_path(s3_path)
     bucket = s3_conn.get_bucket(bucket_name)
 
