4ddcae7ccf4e4963f4098e64fe4b2c400479794b
tweddielin <power6188@gmail.com>
2016-12-21 18:00:02 -0600

    add test for job vectorizing

--- tests/test_job_vectorizers.py
+++ tests/test_job_vectorizers.py
@@ -1,40 +1,27 @@
 from algorithms.job_vectorizers.doc2vec_vectorizer import Doc2Vectorizer
+from algorithms.corpus_creators.basic import GensimCorpusCreator
 from airflow.hooks import S3Hook
 
+from datasets import job_postings
+
 def test_job_vectorizer():
-    sample_document = {
-        "incentiveCompensation": "",
-        "experienceRequirements": "Here are some experience and requirements",
-        "baseSalary": {
-            "maxValue": 0.0,
-            "@type": "MonetaryAmount",
-            "minValue": 0.0
-        },
-        "description": "We are looking for a person to fill this job",
-        "title": "Bilingual (Italian) Customer Service Rep (Work from Home)",
-        "employmentType": "Full-Time",
-        "industry": "Call Center / SSO / BPO, Consulting, Sales - Marketing",
-        "occupationalCategory": "",
-        "qualifications": "Here are some qualifications",
-        "educationRequirements": "Not Specified",
-        "skills": "Customer Service, Consultant, Entry Level",
-        "validThrough": "2014-01-02T00:00:00",
-        "jobLocation": {
-            "@type": "Place",
-            "address": {
-                "addressLocality": "Salisbury",
-                "addressRegion": "PA",
-                "@type": "PostalAddress"
-            }
-        },
-        "@context": "http://schema.org",
-        "alternateName": "Customer Service Representative",
-        "datePosted": "2013-05-12",
-        "@type": "JobPosting"
-    }
     MODEL_NAME = 'gensim_doc2vec'
     PATHTOMODEL = 'skills-private/model_cache/'
     s3_conn = S3Hook().get_conn()
-    job_vec = Doc2Vectorizer(model_name=MODEL_NAME, path=PATHTOMODEL, s3_conn=s3_conn).vectorize(sample_document['description'])
 
-    assert job_vec.shape[0] == 500
+    job_postings_generator = job_postings(s3_conn, '2011Q1')
+    corpus_generator = GensimCorpusCreator().array_corpora(job_postings_generator)
+    #print(corpus_generator.__next__())
+    assert isinstance(corpus_generator.__next__(), list)
+
+
+    vectorized_job_generator = Doc2Vectorizer(model_name=MODEL_NAME,
+                                              path=PATHTOMODEL,
+                                              s3_conn=s3_conn).vectorize(corpus_generator)
+
+    #print(vectorized_job_generator.__next__())
+    assert vectorized_job_generator.__next__().shape[0] == 500
+
+
+
+
