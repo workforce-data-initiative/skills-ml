e58ebef1a4f4e7003ff5cd8d2c3cb0b0d81aa4d7
Tristan Crockett <tristan.h.crockett@gmail.com>
2016-12-27 10:00:41 -0600

    Pin httpretty to prevent test bugs, add s3 util tests

--- utils/s3.py
+++ utils/s3.py
@@ -4,7 +4,7 @@ Common S3 utilities
 import boto
 from config import config
 import logging
-import os
+
 
 def split_s3_path(path):
     """
@@ -26,7 +26,7 @@ def upload(s3_conn, filename, s3_path):
         s3_path: the destination path on s3
     """
     if config.get('local_mode', False):
-        logging.info('Local mode is set, forgoing s3 upload')
+        logging.warning('Local mode is set, forgoing s3 upload')
         return
     bucket_name, prefix = split_s3_path(s3_path)
     bucket = s3_conn.get_bucket(bucket_name)
@@ -35,7 +35,9 @@ def upload(s3_conn, filename, s3_path):
         bucket=bucket,
         name='{}/{}'.format(prefix, filename.replace('/', '_'))
     )
+    logging.info('uploading from %s to %s', filename, key)
     key.set_contents_from_filename(filename)
+
 
 def download(s3_conn, out_filename, s3_path):
     bucket_name, prefix = split_s3_path(s3_path)
@@ -44,8 +46,9 @@ def download(s3_conn, out_filename, s3_path):
         bucket=bucket,
         name=prefix
     )
-    logging.warning('loading from %s into %s', key, out_filename)
+    logging.info('loading from %s into %s', key, out_filename)
     key.get_contents_to_filename(out_filename)
+
 
 def log_download_progress(num_bytes, obj_size):
     logging.info('%s bytes transferred out of %s total', num_bytes, obj_size)
