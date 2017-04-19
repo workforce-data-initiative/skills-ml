b4c05c5d687cc322eae6bacc33db5517e202b567
Tristan Crockett <tristan.h.crockett@gmail.com>
2017-04-19 15:10:01 -0500

    Point towards new README

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import find_packages, setup
 
-with open('README.rst') as readme_file:
+with open('README.md') as readme_file:
     readme = readme_file.read()
 
 with open('requirements.txt') as requirements_file:
