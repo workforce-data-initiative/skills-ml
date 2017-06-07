9e9068bdc3be7ab7e1ee8f77b7bd247a2e4128fc
tweddielin <power6188@gmail.com>
2017-06-07 13:27:42 -0500

    add docstrings

--- skills_ml/algorithms/occupation_classifiers/classifiers.py
+++ skills_ml/algorithms/occupation_classifiers/classifiers.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import json
+from collections import Counter
 
 from gensim.models import Doc2Vec
 
@@ -17,7 +18,25 @@ SYN0 = 'gensim_doc2vec_va_0605.syn0.npy'
 SYN1 = 'gensim_doc2vec_va_0605.syn1.npy'
 
 class SocClassifier(object):
+    """The SocClassifier Object to classify each jobposting description to O*Net SOC code.
+
+    Example:
+
+    Soc = SocClassifier(s3_conn=s3_conn)
+    predicted_soc = Soc.classify(jobposting, mode='top')
+    """
     def __init__(self, model_name=MODEL_NAME, path=PATHTOMODEL, s3_conn=None):
+        """To initialize the SocClassifier Object, the model and lookup disctionary
+        will be downloaded to the tmp/ directory and loaded to the memory.
+
+        Attributes:
+            model_name (str): the name of the model to be used.
+            path (str): the path of the model on S3.
+            s3_conn (:obj: `boto.s3.connection.S3Connection`): the boto object to connect to S3.
+            files (:obj: `list` of (str)): model files need to be downloaded/loaded.
+            model (:obj: `gensim.models.doc2vec.Doc2Vec`): gensim doc2vec model.
+            lookup (dict): lookup table for mapping each jobposting index to soc code.
+        """
         self.model_name = model_name
         self.path = path
         self.s3_conn = s3_conn
@@ -26,6 +45,8 @@ class SocClassifier(object):
         self.lookup = self._load_lookup()
 
     def _load_model(self):
+        """The method to download the model from S3 and load to the memory.
+        """
         if not os.path.isdir('tmp'):
             os.mkdir('tmp')
 
@@ -38,6 +59,8 @@ class SocClassifier(object):
         return Doc2Vec.load('tmp/' + self.model_name)
 
     def _load_lookup(self):
+        """The method to download the lookup dictionary from S3 and load to the memory.
+        """
         filepath = 'tmp/' + LOOKUP
         if not os.path.exists(filepath):
             logging.warning('calling download from %s to %s', self.path + LOOKUP, filepath)
@@ -48,12 +71,20 @@ class SocClassifier(object):
         return lookup
 
     def classify(self, jobposting, mode='top'):
+        """The method to predict the soc code a job posting belongs to.
+
+        Args:
+            jobposting (str): a string of cleaned, lower-cased and pre-processed job description context.
+            mode (str): a flag of which method to use for classifying.
+        """
         inferred_vector = self.model.infer_vector(jobposting.split())
-        resultlist = []
         sims = self.model.docvecs.most_similar([inferred_vector], topn=1)
-        resultlist.append(list(map(lambda l: self.lookup[l], [x[0] for x in sims])))
+        resultlist = list(map(lambda l: self.lookup[l], [x[0] for x in sims]))
         if mode == 'top':
             predicted_soc = resultlist[0]
+
+        if mode == 'common':
+            predicted_soc = [Counter(r).most_common()[0][0] for r in resultlist]
 
         return predicted_soc
 
