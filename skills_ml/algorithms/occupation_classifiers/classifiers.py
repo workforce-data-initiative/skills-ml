f7c69d71a0bebc8face23464eabcd90829b521a9
tweddielin <power6188@gmail.com>
2017-06-13 16:21:31 -0500

    classifiers just handle the classifier selection

--- skills_ml/algorithms/occupation_classifiers/classifiers.py
+++ skills_ml/algorithms/occupation_classifiers/classifiers.py
@@ -3,10 +3,25 @@ import logging
 import json
 from collections import Counter, defaultdict
 
-from gensim.models import Doc2Vec
+from skills_ml.algorithms.occupation_classifiers.nearestneighbor import NearestNeighbors
 
-from sklearn import neighbors
+class Classifiers(object):
+    def __init__(self, classifier_name=None, s3_conn=None):
+        self.classifier_list = ['ann', 'knn', 'svm', 'softmax']
+        self.classifier_name = classifier_name
+        self.s3_conn = s3_conn
+        self.classifier = self._load_classifier()
 
-from skills_utils.s3 import download, split_s3_path, list_files
 
-from skills_ml.algorithms.occupation_classifiers.base import VectorModel
+    def _load_classifier(self):
+        if self.classifier_name == None:
+            return NearestNeighbors(s3_conn=self.s3_conn)
+        else:
+            print('Not implemented yet!')
+            return None
+
+
+    def predict_soc(self, jobposting, **kwargs):
+        return self.classifier.classify(jobposting, **kwargs)
+
+
