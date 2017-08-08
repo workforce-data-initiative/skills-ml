9044b8ba91c53a1d4510cb46d4ec3aa5fee2c0ef
Tristan Crockett <tristan.h.crockett@gmail.com>
2017-08-08 16:25:13 -0500

    Implement File locking [Resolves #93]
    - Require filelock module
    - Use filelocking for both ann index download and CBSA shapefile download

--- skills_ml/datasets/cbsa_shapefile.py
+++ skills_ml/datasets/cbsa_shapefile.py
@@ -1,6 +1,7 @@
 import requests
 import os
 import zipfile
+import filelock
 
 DATASET_URL = 'http://www2.census.gov/geo/tiger/TIGER2015/CBSA/tl_2015_us_cbsa.zip'
 
@@ -15,13 +16,15 @@ def download_shapefile(cache_dir):
 
     Returns: (string) Path to the extracted shapefile
     """
-    write_path = os.path.join(cache_dir, 'tl_2015_us_cbsa.zip')
-    if not os.path.isfile(write_path):
-        response = requests.get(DATASET_URL, stream=True)
-        with open(write_path, 'wb') as fd:
-            for chunk in response.iter_content(chunk_size=128):
-                fd.write(chunk)
-    zip_ref = zipfile.ZipFile(write_path, 'r')
-    zip_ref.extractall(cache_dir)
-    zip_ref.close()
+    lock = filelock.FileLock(os.path.join(cache_dir, 'cbsa_shp_dl.lock'))
+    with lock.acquire(timeout=1000):
+        write_path = os.path.join(cache_dir, 'tl_2015_us_cbsa.zip')
+        if not os.path.isfile(write_path):
+            response = requests.get(DATASET_URL, stream=True)
+            with open(write_path, 'wb') as fd:
+                for chunk in response.iter_content(chunk_size=128):
+                    fd.write(chunk)
+        zip_ref = zipfile.ZipFile(write_path, 'r')
+        zip_ref.extractall(cache_dir)
+        zip_ref.close()
     return os.path.join(cache_dir, 'tl_2015_us_cbsa.shp')
