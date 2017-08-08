4b6ad56a292df3828a7b1e373d6cf0d143b58f59
tweddielin <tweddielin@gmail.com>
2017-08-08 16:39:55 -0500

    replace hard-coded /tmp with a global variable

--- skills_ml/algorithms/occupation_classifiers/base.py
+++ skills_ml/algorithms/occupation_classifiers/base.py
@@ -8,6 +8,7 @@ from gensim.models import Doc2Vec
 
 from skills_utils.s3 import download, split_s3_path, list_files
 
+LOCAL_CACHE_DIRECTORY = 'tmp'
 
 class VectorModel(object):
     """The VectorModel Object is a base object which specifies which word-embeding model to be used in
@@ -63,7 +64,7 @@ class VectorModel(object):
             gensim.models.doc2vec.Doc2Vec: The word-embedding model object.
         """
         try:
-            model = Doc2Vec.load('tmp/' + self.model_name)
+            model = Doc2Vec.load(os.path.join(LOCAL_CACHE_DIRECTORY, self.model_name))
             return model
 
         except:
@@ -78,14 +79,14 @@ class VectorModel(object):
                     model = Doc2Vec.load(os.path.join(td, self.model_name))
 
             else:
-                if not os.path.isdir('tmp'):
-                    os.mkdir('tmp')
+                if not os.path.isdir(LOCAL_CACHE_DIRECTORY):
+                    os.mkdir(LOCAL_CACHE_DIRECTORY)
                 for f in files:
-                    filepath = 'tmp/' + f
+                    filepath = os.path.join(LOCAL_CACHE_DIRECTORY, f)
                     if not os.path.exists(filepath) and self.saved:
                         logging.warning('calling download from %s to %s', self.s3_path + f, filepath)
                         download(self.s3_conn, filepath, os.path.join(self.s3_path, f))
-                model = Doc2Vec.load('tmp/' + self.model_name)
+                model = Doc2Vec.load(os.path.join(LOCAL_CACHE_DIRECTORY, self.model_name))
 
             return model
 
@@ -96,7 +97,7 @@ class VectorModel(object):
             dict: a lookup table for mapping gensim index to soc code.
         """
         try:
-            filepath = 'tmp/' + self.lookup_name
+            filepath = os.path.join(LOCAL_CACHE_DIRECTORY, self.lookup_name)
             with open(filepath, 'r') as handle:
                 lookup = json.load(handle)
             return lookup
@@ -112,7 +113,7 @@ class VectorModel(object):
                         lookup = json.load(handle)
 
             else:
-                filepath = 'tmp/' + self.lookup_name
+                filepath = os.path.join(LOCAL_CACHE_DIRECTORY, self.lookup_name)
                 if not os.path.exists(filepath):
                     logging.warning('calling download from %s to %s', self.s3_path + self.lookup_name, filepath)
                     download(self.s3_conn, filepath , os.join(self.s3_path, self.lookup_name))
