3953f1c55d330098f07a03d40a2d1bf759959755
tweddielin <tweddielin@gmail.com>
2017-10-31 14:46:24 -0500

    add safe_get() in util

--- skills_ml/utils/__init__.py
+++ skills_ml/utils/__init__.py
@@ -1,1 +1,18 @@
 """Common utilities"""
+
+def safe_get(dct, *keys):
+	"""Extract value from nested dictionary
+	Args:
+		dct (dict): dictionary one want to extrat value from
+		*keys (string|list|tuple): keys to exatract value
+
+	Returns:
+		value
+	"""
+	for key in keys:
+		try:
+			dct = dct[key]
+		except:
+			return None
+	return dct
+
