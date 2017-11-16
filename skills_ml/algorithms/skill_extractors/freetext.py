422f31b01b2f4f7aa1c8390aee51f19042768653
Tristan Crockett <tristan.h.crockett@gmail.com>
2017-11-15 22:11:48 -0600

    WIP

--- skills_ml/algorithms/skill_extractors/freetext.py
+++ skills_ml/algorithms/skill_extractors/freetext.py
@@ -1,8 +1,27 @@
 import csv
 import logging
 from collections import Counter, defaultdict
+import smart_open
+
+import nltk
+try:
+    nltk.sent_tokenize('test')
+except LookupError:
+    nltk.download('punkt')
 
 from skills_ml.algorithms.string_cleaners import NLPTransforms
+
+from skills_ml.algorithms.string_cleaners import NLPTransforms
+from skills_ml.algorithms.corpus_creators.basic import SimpleCorpusCreator
+
+import json
+from collections import Counter, OrderedDict, defaultdict
+import uuid
+
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+
+import re
 
 
 class FreetextSkillExtractor(object):
@@ -10,6 +29,7 @@ class FreetextSkillExtractor(object):
 
     Originally written by Kwame Porter Robinson
     """
+    name = 'onet_ksat_exact'
     def __init__(self, skills_filename):
         self.skills_filename = skills_filename
         self.tracker = {
@@ -86,6 +106,7 @@ class OccupationScopedSkillExtractor(FreetextSkillExtractor):
     """Extract skills from unstructured text,
     but only return matches that agree with a known taxonomy
     """
+    name = 'onet_ksat_occscoped_exact'
     def _skills_lookup(self):
         """Create skills lookup
 
@@ -116,3 +137,126 @@ class OccupationScopedSkillExtractor(FreetextSkillExtractor):
             All values set to 1 (multiple occurrences of a skill do not count)
         """
         return self._document_skills_in_lookup(document, self.lookup[soc_code])
+
+
+class FuzzySkillExtractor(FreetextSkillExtractor):
+    name = 'onet_ksat_fuzzy'
+    def reg_ex(self, s):
+        s = s.replace(".","\.")
+        s = s.replace("^","\^")
+        s = s.replace("$","\$")
+        s = s.replace("*","\*")
+        s = s.replace("+","\+")
+        s = s.replace("?","\?")
+        return s
+
+    def _skills_lookup(self):
+        """Create skills lookup
+
+        Reads the object's filename containing skills into a lookup
+
+        Returns: (set) skill names
+        """
+        with open(self.skills_filename) as infile:
+            reader = csv.reader(infile, delimiter='\t')
+            header = next(reader)
+            index=3
+            generator = (self.reg_ex(row[index]) for row in reader)
+            
+            return set(generator)
+
+    def ie_preprocess(self, document):
+        """This function takes raw text and chops and then connects the process to break     
+           it down into sentences"""
+        
+        # Pre-processing
+        # e.g.","exempli gratia"
+        document=document.replace("e.g.","exempli gratia")
+        
+        # Sentence tokenizer out of nltk.sent_tokenize
+        split = re.split('\n|\*', document)
+        
+        # Sentence tokenizer
+        sentences=[]
+        for sent in split:
+            sents = nltk.sent_tokenize(sent)
+            length = len(sents)
+            if length == 0:
+                next
+            elif length == 1:
+                sentences.append(sents[0])
+            else:
+                for i in range(length):
+                    sentences.append(sents[i])
+        return sentences
+
+    def candidate_skills(self, document):
+        sentences = self.ie_preprocess(document)
+        
+        skills = defaultdict(list)
+        for skill in available_skills:
+            len_skill = len(skill.split())
+            for sent in sentences:
+                sent = sent.encode('utf-8')
+                
+                #Exact matching
+                if len_skill ==1:
+                    sent = sent.decode('utf-8')
+                    if re.search(r'\b'+skill +r'\b', sent, re.IGNORECASE):
+                        skills[skill] = [100]
+                        skills[skill].append(sent)
+                #Fuzzy matching
+                else:        
+                    ratio = fuzz.partial_ratio(skill,sent)
+                    # You can adjust the partial of matching here: 100 => exact matching 0 => no matching
+                    if ratio > 88:
+                        skills[skill] = [ratio]
+                        skills[skill].append(sent)
+        return skills
+
+class Sample(object):
+    def __init__(self, base_path, sample_name):
+        self.base_path = base_path
+        self.sample_name = sample_name
+        self.full_path = '/'.join([self.base_path, sample_name])
+
+    def __iter__(self):
+        with smart_open(self.full_path) as f:
+            for line in f:
+                yield line
+
+def generate_skill_candidates(candidates_path, sample, skill_extractor):
+    corpus_creator = SimpleCorpusCreator()
+    corpus_creator.document_schema_fields = ['description']
+    for job_posting_string in sample:
+        job_posting = json.loads(job_posting_string)
+        document = corpus_creator._join(job_posting)
+        candidate_skills = skill_extractor.candidate_skills(document)
+        for candidate_skill, metadata in candidate_skills.items():
+            cs_id = str(uuid.uuid4())
+            candidate_filename = '/'.join([candidates_path, sample.name, skill_extractor.name, cs_id])
+            with open(candidate_filename, 'w') as candidate_file:
+                candidate_metadata = {
+                    'job_posting_id': job_posting['id'],
+                    'candidate_skill': candidate_skill,
+                    'confidence': metadata[0],
+                    'context': metadata[1],
+                    'skill_extractor_name': skill_extractor.name
+                }
+                json.dump(obj, candidate_file)
+        
+
+# TODO: define name attribute on skill extractor
+
+# one invocation:
+# get 24k sample into list
+# call skill candidate generator with 24k sample and exact match
+# 
+# call skill candidate generator
+
+if __name__ == '__main__':
+    sample_path_config = 'open-skills-private/sampled_jobpostings'
+    candidates_path_config = 'open-skills-private/skill_candidates'
+    sample = Sample(sample_path_config, 'samples_300_v1')
+    skill_extractor = FuzzySkillExtractor("output/skills_master_table.tsv")
+    generate_skill_candidates(candidates_path_config, sample, skill_extractor)
--- skills_ml/algorithms/skill_extractors/fuzzyMatching_fast.py
+++ skills_ml/algorithms/skill_extractors/fuzzyMatching_fast.py
@@ -1,5 +1,5 @@
-from string_cleaners import NLPTransforms
-from corpus_creators.basic import SimpleCorpusCreator
+from skills_ml.algorithms.string_cleaners import NLPTransforms
+from skills_ml.algorithms.corpus_creators.basic import SimpleCorpusCreator
 
 import json
 from collections import Counter, OrderedDict, defaultdict
@@ -14,8 +14,13 @@ import re
 
 
 
-f = open('random200_NLX_b8384025-fa09-417d-ae10-96880fac86be','r')
-skills_filename = "skills_master_table.tsv"
+try:
+    nltk.sent_tokenize('test')
+except LookupError:
+    nltk.download('punkt')
+
+f = open('VA_caac4ea4-809b-4938-b339-8c959f5c3c81','r')
+skills_filename = "output/skills_master_table.tsv"
 
 #out = open("NLX_2011Q1_description.txt", "wb")
 
@@ -36,24 +41,6 @@ def skills_lookup(skills_filename):
         generator = (reg_ex(row[index]) for row in reader)
         
         return set(generator)
-    
-    #QUESTION
-    #index = header.index(self.nlp.transforms[0])
-    
-    #find longest list
-    #long_l = [row[index] for row in reader]
-    #print (max(long_l, key=len))
-    #digital imaging communications in medicine dicom-compatible image acquisition and integration software products
-    #for row in reader:
-     #   skill = row[3]
-      #  words = skill.split()
-       # result = skill+"\t"+str(len(words))
-        #print(result)   
-
-    #generator = (reg_ex(row[index]) for row in reader)
-
-    #lookup = set[generator]
-
 
 def ie_preprocess(document):
     """This function takes raw text and chops and then connects the process to break     
@@ -67,96 +54,55 @@ def ie_preprocess(document):
     split = re.split('\n|\*', document)
     
     # Sentence tokenizer
-    #sentences = [nltk.sent_tokenize(sent) for sent in split]
     sentences=[]
     for sent in split:
         sents = nltk.sent_tokenize(sent)
         length = len(sents)
-        if length ==0:
+        if length == 0:
             next
         elif length == 1:
             sentences.append(sents[0])
         else:
             for i in range(length):
                 sentences.append(sents[i])
-                
-            
-            
-        
-    
-    #try:
-     #   sentences = nltk.sent_tokenize(document.encode('utf-8'))
-    #except:
-     #   sentences = nltk.sent_tokenize(document)
-    
-    #sentences = [nltk.word_tokenize(sent) for sent in sentences]
-    #sentences = [nltk.wordpunct_tokenize(sent) for sent in sentences]
-    
-    #sentences = [nltk.pos_tag(sent) for sent in sentences]
     return sentences
 
-for line in f:
 
-    data = json.loads(line, object_pairs_hook=OrderedDict)
+def generate_candidates(job_postings):
+    available_skills = skills_lookup(
+    corpus_creator = SimpleCorpusCreator()
+    corpus_creator.document_schema_fields = ['description']
+    for line in job_postings:
+        data = json.loads(line, object_pairs_hook=OrderedDict)
+        document = corpus_creator._join(data)
+        sentences = ie_preprocess(document)
         
-    #SimpleCorpusCreator
-    #document = SimpleCorpusCreator()._transform(data)
-    document = SimpleCorpusCreator()._join(data)
-    
-    
-    #document = document.replace("\n", ". ")
-    #document = document.replace("*", ". *")
-    sentences = ie_preprocess(document)
-    
-    #skills = Counter()
-    skills = defaultdict(list)
-    num=0
-    for skill in skills_lookup(skills_filename):
-    #for skill in lookup:
-    
-        
-        #print(str(num))
-        #print(skill)
-        len_skill = len(skill.split())
-        for sent in sentences:
-            sent = sent.encode('utf-8')
-            #print(skill+"\t"+sent)
-            #sent = sent.encode('utf-8')
+        skills = defaultdict(list)
+        num=0
+        for skill in available_skills:
+            len_skill = len(skill.split())
+            for sent in sentences:
+                sent = sent.encode('utf-8')
+                
+                #Exact matching
+                if len_skill ==1:
+                    sent = sent.decode('utf-8')
+                    if re.search(r'\b'+skill +r'\b', sent, re.IGNORECASE):
+                        skills[skill] = [100]
+                        skills[skill].append(sent)
+                #Fuzzy matching
+                else:        
+                    ratio = fuzz.partial_ratio(skill,sent)
+                    # You can adjust the partial of matching here: 100 => exact matching 0 => no matching
+                    if ratio > 88:
+                        skills[skill] = [ratio]
+                        skills[skill].append(sent)
             
-            #Exact matching
-            if len_skill ==1:
-                sent = sent.decode('utf-8')
-                if re.search(r'\b'+skill +r'\b', sent, re.IGNORECASE):
-                    skills[skill] = [100]
-                    skills[skill].append(sent)
-            #Fuzzy matching
-            else:        
-                ratio = fuzz.partial_ratio(skill,sent)
-                # You can adjust the partial of matching here: 100 => exact matching 0 => no matching
-                if ratio > 88:
-                    skills[skill] = [ratio]
-                    skills[skill].append(sent)
-        
-        num +=1
-    #print ("DONE!")
-    #print(skills)
-    for key, values in skills.items():
-        for v in values:
-            try:
-                print (key," : ", v)
-            except:
-                print (key," : ", v.encode('utf-8'))
-
-    
-    
-
-    
-    #out.write(id_as_bytes)    
-    #out.write(description)
-    #print (description)
-#out.close()
-
-
-
-    
+            num +=1
+        for key, values in skills.items():
+            for v in values:
+                try:
+                    print (key," : ", v)
+                except:
+                    print (key," : ", v.encode('utf-8'))
 
