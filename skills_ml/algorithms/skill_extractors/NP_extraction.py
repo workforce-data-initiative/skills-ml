d22c53a90123c931993a56fa77a73309f207d3e6
Tristan Crockett <tristan.h.crockett@gmail.com>
2018-02-09 17:32:54 -0600

    Add noun phrase ending extractor classes and test for the skills one

--- skills_ml/algorithms/skill_extractors/NP_extraction.py
+++ skills_ml/algorithms/skill_extractors/NP_extraction.py
@@ -1,200 +1,178 @@
 # coding: utf-8
-import json
-from collections import OrderedDict
-import io
+import logging
 import re
-from collections import defaultdict
-import operator
+from collections import Counter
 
 import nltk
-import csv
-from operator import itemgetter
+try:
+    nltk.pos_tag('test')
+except LookupError:
+    nltk.download('averaged_perceptron_tagger')
 
-import time
-
-import collections
-
-f = open('NLX_b8384025-fa09-417d-ae10-96880fac86be','r')
-#f = open('NLX_100','r')
-
-np_dic={}
+from .base import SkillExtractor
 
 
-def np_extraction(candidates):
-    for c in candidates:
-        c.strip()
-        if c != "\n":
-            #print(c.encode("utf-8"))
-            output = ie_preprocess(c)
-            
-            grammar = r"""
-                      NP: {<JJ.*>*<NN.*><NN.*>*}   # chunk adjectives and nouns
-                       """
-            cp = nltk.RegexpParser(grammar)
-            for sent in output:
-                #result = cp.parse(sent)
-                #tree = cp.parse(result)
-                if sent:
-                    tree = cp.parse(sent)
-                    for subtree in tree.subtrees():
-                        if subtree.label() == 'NP': 
-                            
-                            np=""
-        
-                            for node in subtree:
-                                try:
-                                    np += node[0].encode('utf-8') + ' '
-                                except:
-                                    np += node[0] + ' '
-                            np.strip()
-                            #s = `id_cnt` +"\t" + np
-                            #print np
-                            
-                            if not np in np_dic:
-                                np_dic.update({np:1})
-                            else:
-                                np_dic[np] +=1
-                                
-def np_extraction2(c):
-
-    c.strip()
-    if c != "\n":
-        #print(c.encode("utf-8"))
-        output = ie_preprocess(c)
-        
-        grammar = r"""
-                  NP: {<JJ.*>*<NN.*><NN.*>*}   # chunk adjectives and nouns
-                   """
-        cp = nltk.RegexpParser(grammar)
-        for sent in output:
-            #result = cp.parse(sent)
-            #tree = cp.parse(result)
-            if sent:
-                tree = cp.parse(sent)
-                for subtree in tree.subtrees():
-                    if subtree.label() == 'NP': 
-                        
-                        np=""
-    
-                        for node in subtree:
-                            try:
-                                np += node[0].encode('utf-8') + ' '
-                            except:
-                                np += node[0] + ' '
-                        np.strip()
-                        #s = `id_cnt` +"\t" + np
-                        #print np
-                        
-                        if not np in np_dic:
-                            np_dic.update({np:1})
-                        else:
-                            np_dic[np] +=1
-
-
-def ie_preprocess(document):
-    """This function takes raw text and chops and then connects the process to break     
+def sentences_words_pos(document):
+    """This function takes raw text and chops and then connects the process to break
        it down into sentences, then words and then complete part-of-speech tagging"""
     try:
         sentences = nltk.sent_tokenize(document.encode('utf-8'))
     except:
         sentences = nltk.sent_tokenize(document)
-    #sentences = [nltk.word_tokenize(sent) for sent in sentences]
     sentences = [nltk.wordpunct_tokenize(sent) for sent in sentences]
-    
+
     sentences = [nltk.pos_tag(sent) for sent in sentences]
     return sentences
 
 
+def count_noun_phrases_in_line(line, counts):
+    if line.strip() != "\n":
+        output = sentences_words_pos(line)
+
+        grammar = r"""
+                  NP: {<JJ.*>*<NN.*><NN.*>*}   # chunk adjectives and nouns
+                   """
+        cp = nltk.RegexpParser(grammar)
+        for sent in output:
+            if sent:
+                tree = cp.parse(sent)
+                for subtree in tree.subtrees():
+                    if subtree.label() == 'NP':
+                        np = ""
+                        for node in subtree:
+                            try:
+                                np += node[0].encode('utf-8') + ' '
+                            except:
+                                np += node[0] + ' '
+                        np = np.strip()
+
+                        logging.info('Adding %s', np)
+                        counts[np] += 1
 
 
-def first_word(desc_line):
-    indicators={}
-    sorted_indicators={}
-    for c in desc_line:
-        c.strip()
-        if c != "\n":
-            #indicators['first']=c[1]
-            first=c.split(' ',1)[0]
-            first = first.replace('\n','')
-            if not first in indicators:
-                indicators.update({first:1})
-            else:
-                indicators[first] +=1
-    total = sum(indicators.values())
-    sorted_indicators = sorted(indicators.items(), key = operator.itemgetter(1),reverse = True)
+BULLETS = ['+', '*', '-']
+
+
+def is_bulleted(string):
+    string_first = string.split(' ', 1)[0].replace('\n', '')
     try:
-        most = sorted_indicators[0][0]
-        most_value= sorted_indicators[0][1]
+        if string_first[0] in BULLETS:
+            return True
+        else:
+            return False
     except:
-        most = 'Z'
-    return most
-
-total_dic={}
-line_num=0
-
-start = time.time()
-print("Starting time: %.2f" %start)
-
-c = collections.Counter()
-
-for line in f:
-    line_num +=1
-    #print line_num
-    
-    data = json.loads(line, object_pairs_hook=OrderedDict)
-    #keys=data.keys()
-    job_id= data["id"]
-    title = data["title"]
-    soc = data["onet_soc_code"]
-    soc_code = soc[:2]
-    skills = data["skills"]
-    #print(title)
-    try:
-        description = data["description"]
-    except:
-        description = data[""]
-        #print(value)
-        #print(description.encode('utf-8'))
-    #description_list= description.split()
-    
-
-    #sentence tokenization
-    desc_line = re.findall(r'\n.*', description)
-    #desc_line = re.findall(r'\n', description)
-    
-    
-    
-    num=len(desc_line)
-    #First Character Checking
-
-    most_char=first_word(desc_line)
-    bullet =['+', '*','-']
-    for d in desc_line:
-        d_first=d.split(' ',1)[0]
-        d_first = d_first.replace('\n','')
-        try:
-            if d_first[0] in bullet:
-                np_extraction2(d)
-        except:
-            next
-        
-    
-    # NOUN PHRASE CHUNKING
-    #np_extraction(desc_line)
-end = time.time()
-print ("Elasped time: %.2f" %(end-start))
+        return False
 
 
-with open('NLX_2011Q_all_out.csv', 'wb') as f:
-#with open('NLX_100_out.csv', 'wb') as f:    
-    w = csv.writer(f)
-    for key, value in np_dic.items():
-        w.writerow([key, value])
+BEGINNING_CHARS_TO_REMOVE = [
+    "+ ",
+    "â €¢ ",
+    "Â · ",
+    "Â · Â Â Â Â Â Â Â ",
+    "\. ",
+    "\- ",
+    "/ ",
+    "**,** ",
+    "** ",
+    "* ",
+    "á ",
+    "### ",
+    "‰ Û¢ ",
+    "å á å å å å å å å ",
+    "å á ",
+    "á "
+]
 
-#for x in indicators:
-#    print("%s : %d  " % (x.encode('utf-8'), indicators[x]))
-#for x in np_dic:
-#    try:
-#        print("%s : %d  " % (x.encode('utf-8'), np_dic[x]))
-#    except:
-#        print("%s : %d  " % (x, np_dic[x]))
+
+def clean_beginning(term):
+    for c in BEGINNING_CHARS_TO_REMOVE:
+        if term.startswith(c):
+            term = term.replace(c, "")
+            break
+    return term
+
+
+class NPEndPatternExtractor(SkillExtractor):
+    def __init__(self, endings, stop_phrases, only_bulleted_lines=True, *args, **kwargs):
+        self.endings = endings
+        self.stop_phrases = stop_phrases
+        self.only_bulleted_lines = only_bulleted_lines
+
+    def document_skill_counts(self, document):
+        """Count skills in the document
+
+        Args:
+            document (string) A document for searching, such as a job posting
+
+        Returns: (collections.Counter) skills found in the document, all
+            values set to 1 (multiple occurrences of a skill do not count)
+        """
+        noun_phrase_counts = Counter()
+        lines = re.findall(r'\n.*', document)
+        for line in lines:
+            if not self.only_bulleted_lines or is_bulleted(line):
+                count_noun_phrases_in_line(line, noun_phrase_counts)
+        skill_counts = Counter()
+        for phrase, frequency in noun_phrase_counts.items():
+            term_list = phrase.split()
+            if term_list[-1].lower() in self.endings:
+                    cleaned_phrase = clean_beginning(phrase).lower()
+                    if cleaned_phrase not in self.stop_phrases:
+                        skill_counts[cleaned_phrase] += frequency
+        return skill_counts
+
+
+class SkillPatternExtractor(NPEndPatternExtractor):
+    GENERIC_SKILL_PHRASES = set([
+        'advanced skills',
+        'basic skills',
+        'bonus skills',
+        'career skills',
+        'core skills',
+        'demonstrable skill',
+        'demonstrated skill',
+        'demonstrated skills',
+        'desired additional skills',
+        'desired skills',
+        'encouraged skills',
+        'essential skills',
+        'following requisite skills',
+        'following skill',
+        'following skills',
+        'individual skills',
+        'job skills',
+        'key skills',
+        'new skills',
+        'or skill',
+        'other skills',
+        'personal skills',
+        'preferred skills',
+        'primary skills',
+        'professional skills',
+        'related skills',
+        'required job skills',
+        'required skills',
+        'skill',
+        'skills',
+        'special skills',
+        'specific skills',
+        'welcomed skill',
+    ])
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(
+            endings=['skills', 'skill'],
+            stop_phrases=self.GENERIC_SKILL_PHRASES,
+            *args,
+            **kwargs
+        )
+
+
+class AbilityPatternExtractor(NPEndPatternExtractor):
+    def __init__(self, *args, **kwargs):
+        super().__init__(
+            endings=['ability', 'abilities'],
+            stop_phrases=[],
+            *args,
+            **kwargs
+        )
