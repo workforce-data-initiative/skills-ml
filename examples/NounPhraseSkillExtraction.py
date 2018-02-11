b2f3e6f0324d6913307c1c8fe57f3163b3a462c2
Tristan Crockett <tristan.h.crockett@gmail.com>
2018-02-10 20:13:11 -0600

    Add intro comment to noun phrase skill extraction example

--- examples/NounPhraseSkillExtraction.py
+++ examples/NounPhraseSkillExtraction.py
@@ -1,3 +1,9 @@
+# Extracting skills using noun phrase endings
+#
+# To showcase the noun phrase skill extractor, we download open job postings
+# from Virginia Tech's open data portal and run them through the skill extractor.
+# In the end, we have the most commonly occurring noun phrases ending in
+# 'skill' or 'skills'.
 from collections import Counter
 import json
 import logging
@@ -11,7 +17,7 @@ VT_DATASET_URL = 'http://opendata.cs.vt.edu/dataset/ab0abac3-2293-4c9d-8d80-22d4
 
 
 # VT job postings do not include line breaks, so the bulleted-line filter
-# will be too restrictive. let's turn it off
+# will remove all possible matches. Let's turn it off
 pattern_extractor = SkillEndingPatternExtractor(only_bulleted_lines=False)
 
 logging.info('Downloading sample Virginia Tech open jobs file')
