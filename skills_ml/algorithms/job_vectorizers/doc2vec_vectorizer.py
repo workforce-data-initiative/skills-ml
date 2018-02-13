a8d8f827cb87a039b0a464630a24548b10b2c35b
Eddie Lin <tweddielin@gmail.com>
2018-02-13 10:53:46 -0600

    Dev embedding feature (#121)
    * implement emebedding model feature
    
    * add correct path of model
    
    * use map reduce to create feature combination
    
    * add unitest
    
    * use mock_s3_deprecated
    
    * changes for PR
    
    * remove if statement for checking object type

--- skills_ml/algorithms/job_vectorizers/doc2vec_vectorizer.py
+++ skills_ml/algorithms/job_vectorizers/doc2vec_vectorizer.py
@@ -6,16 +6,17 @@ from skills_ml.algorithms.string_cleaners import NLPTransforms
 
 from skills_utils.s3 import download
 
-MODEL_NAME = 'gensim_doc2vec'
-PATHTOMODEL = 'skills-private/model_cache/'
+MODEL_NAME = 'gensim_doc2vec_va_0605'
+PATHTOMODEL = 'open-skills-private/model_cache/va_0605/'
 
 
 class Doc2Vectorizer(object):
-    def __init__(self, model_name=MODEL_NAME, path=PATHTOMODEL, s3_conn=None):
-        self.model_name = model_name
-        self.path = path
+    def __init__(self, model_name=None, path=None, s3_conn=None):
+        self.model_name = model_name or MODEL_NAME
+        self.path = path or PATHTOMODEL
         self.nlp = NLPTransforms()
         self.s3_conn = s3_conn
+        self.model = self._load_model(modelname=self.model_name)
 
     def _load_model(self, modelname):
         if not os.path.isdir('tmp'):
@@ -25,9 +26,10 @@ class Doc2Vectorizer(object):
         if not os.path.exists(filepath):
             logging.warning('calling download from %s to %s', s3path, filepath)
             download(self.s3_conn, filepath, s3path)
+        else:
+            logging.warning('model existed in tmp/')
         return Doc2Vec.load(filepath)
 
     def vectorize(self, documents):
-        model = self._load_model(modelname=self.model_name)
         for document in documents:
-            yield model.infer_vector(document)
+            yield self.model.infer_vector(document)
--- skills_ml/algorithms/skill_feature_creator/__init__.py
+++ skills_ml/algorithms/skill_feature_creator/__init__.py
@@ -1,81 +1,118 @@
+from functools import reduce
+
+from skills_ml.algorithms.job_vectorizers.doc2vec_vectorizer import Doc2Vectorizer
+
 class FeatureCreator(object):
     """ Feature Creator Factory that help users to instantiate different
     types of feature at once and combine them together.
 
     Example:
-        doc = "some job posting..."
-        feature_vector = FeatureCreate(doc).combine(features="all")
-        feature_vector = FeatureCreate(doc).combine(features=["StructuralFeature", "EmbeddingFeature"])
+        from airflow.hooks import S3Hook
+        from skills_ml.algorithms.skill_feature_creator import FeatureCreator
+
+        s3_conn = S3Hook().get_conn()
+
+        docs=["something", "something2"]
+
+        feature_vector_generator = FeatureCreator(s3_conn, features="all").transform(docs)
+        feature_vector_generator = FeatureCreator(s3_conn, features=["StructuralFeature", "EmbeddingFeature"]).transform(docs)
 
     Args:
         doc (string): job posting data.
     """
-    def __init__(self, doc):
-        self.all_features = [ f.__name__ for f in FeatureCreator.__subclasses__()]
-        self.doc = doc
+    def __init__(
+            self,
+            s3_conn=None,
+            features="all",
+            embedding_model_name=None,
+            embedding_model_path=None):
+        self.all_features = [ f.__name__ for f in FeatureFactory.__subclasses__()]
+        self.params = {
+            "s3_conn": s3_conn,
+            "embedding_model_name": embedding_model_name,
+            "embedding_model_path": embedding_model_path
+        }
+        self.features = features
 
-    @staticmethod
-    def _factory(type, doc):
-        if type == "StructuralFeature":
-            return StructuralFeature(doc)
+    @property
+    def selected_features(self):
+        if self.features == "all":
+            return self.all_features
+        elif isinstance(self.features, list):
+            if not (set(self.features) < set(self.all_features)):
+                not_supported = list(set(self.features) - set(self.all_features))
+                raise Exception("\"{}\" not supported!".format(", ".join(not_supported)))
+            else:
+                return self.features
+        else:
+            raise Exception(TypeError)
 
-        if type == "ContextualFeature":
-            return ContextualFeature(doc)
-
-        if type == "EmbeddingFeature":
-            return EmbeddingFeature(doc)
-
-    def combine(self, features="all"):
-        """ Methods to combine different type of features.
+    def transform(self, docs, combine="concat"):
+        """ Methods to combine different types of feature and transform to a vector as a generator.
         We might want to have different way to combine vectors in the future (eg. concat, average...).
 
         Args:
+            docs (iterator): jobposting iterator
             features (str or list): If it's "all", it will return the combined result of all types of features.
                                     If it's a list of feature types, it will return the combined result of the feature
                                     types that one specifies.
         Return:
-            (list): a combined feature vector
+            (generator): a combined feature vector generator
         """
-        if features == "all":
-            features_to_be_combined = self.all_features
-        elif isinstance(features, list):
-            if not (set(features) < set(self.all_features)):
-                not_supported = list(set(features) - set(self.all_features))
-                raise Exception("{} not supported!".format(", ".join(not_supported)))
-            else:
-                features_to_be_combined = features
+        features_to_be_combined = self.selected_features
+        feature_objects = [FeatureFactory._factory(feature, **self.params) for feature in features_to_be_combined]
+        if combine == "concat":
+            for doc in docs:
+                yield reduce(lambda x, y: x+y, map(lambda x: x.output(doc), feature_objects))
         else:
-            raise Exception(TypeError)
-
-        result = []
-        for feature in features_to_be_combined:
-            result.extend(FeatureCreator._factory(feature, self.doc).output())
-        return result
+            raise Exception("\"{}\" not supported!".format(combine))
 
 
-class StructuralFeature(FeatureCreator):
+class FeatureFactory(object):
+    @staticmethod
+    def _factory(type, **kwargs):
+        if type == "StructuralFeature":
+            return StructuralFeature()
+
+        if type == "ContextualFeature":
+            return ContextualFeature()
+
+        if type == "EmbeddingFeature":
+            return EmbeddingFeature(**kwargs)
+
+        else:
+            raise ValueError('Bad feature type \"{}\"'.format(type))
+
+
+class StructuralFeature(FeatureFactory):
     """ Sturctural features
     """
-    def output(self):
-        """ Output a feature vector. Now it's just a simple example.
+    def output(self, doc):
+        """ Output a feature vector. Need to be implemented! Now it's just a simple example.
         """
-        return ["StructuralFeature" + "_" + self.doc]
+        return ["StructuralFeature_" + doc]
 
 
-class ContextualFeature(FeatureCreator):
+class ContextualFeature(FeatureFactory):
     """ Contextual features
     """
-    def output(self):
-        """ Output a feature vector. Now it's just a simple example.
+    def output(self, doc):
+        """ Output a feature vector. Need to be implemented! Now it's just a simple example.
         """
-        return ["ContextualFeature" + "_" + self.doc]
+        return ["ContextualFeature_" + doc]
 
 
-class EmbeddingFeature(FeatureCreator):
+class EmbeddingFeature(FeatureFactory):
     """ Embedding features
     """
-    def output(self):
-        """ Output a feature vector. Now it's just a simple example.
-        """
-        return ["EmbeddingFeature" + "_" + self.doc]
+    def __init__(self, **kwargs):
+        super().__init__()
+        model_name = kwargs["embedding_model_name"]
+        path = kwargs["embedding_model_path"]
+        s3_conn = kwargs["s3_conn"]
+        self.embedding_model = Doc2Vectorizer(model_name, path, s3_conn)
 
+    def output(self, doc):
+        """ Output a feature vector.
+        """
+        return list(list(self.embedding_model.vectorize([doc]))[0])
