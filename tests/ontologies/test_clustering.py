3fc83d8e7d10053bf0dc9326cd7c9d19c8002941
tweddielin <tweddielin@gmail.com>
2018-10-22 11:35:41 -0500

    fix test clustering

--- tests/ontologies/test_clustering.py
+++ tests/ontologies/test_clustering.py
@@ -16,10 +16,10 @@ class TestClustering(unittest.TestCase):
         # are occupations within that major group
 
         major_group_37_concept= list(onet.filter_by(lambda edge: edge.occupation.identifier == '37').occupations)[0]
-        major_group_37_entities = onet.filter_by(lambda edge: edge.occupation.identifier[:3] == '37-').occupations
+        major_group_37_entities = list(onet.filter_by(lambda edge: edge.occupation.identifier[:3] == '37-').occupations)
 
         major_group_35_concept= list(onet.filter_by(lambda edge: edge.occupation.identifier == '35').occupations)[0]
-        major_group_35_entities = onet.filter_by(lambda edge: edge.occupation.identifier[:3] == '35-').occupations
+        major_group_35_entities = list(onet.filter_by(lambda edge: edge.occupation.identifier[:3] == '35-').occupations)
 
         occupation_clustering = Clustering(
                 name="test_major_group_occupations",
@@ -33,9 +33,9 @@ class TestClustering(unittest.TestCase):
         assert len(occupation_clustering) == 2
         assert set(occupation_clustering.keys()) == set([major_group_37_concept.name, major_group_35_concept.name])
         assert occupation_clustering["Building and Grounds Cleaning and Maintenance"] == major_group_37_entities
-        assert occupation_clustering.map_original_key["Building and Grounds Cleaning and Maintenance"] == major_group_37_concept
+        assert occupation_clustering.map_raw_key["Building and Grounds Cleaning and Maintenance"] == major_group_37_concept
         assert occupation_clustering["Food Preparation and Serving Related Occupations"] == major_group_35_entities
-        assert occupation_clustering.map_original_key["Food Preparation and Serving Related Occupations"] == major_group_35_concept
+        assert occupation_clustering.map_raw_key["Food Preparation and Serving Related Occupations"] == major_group_35_concept
 
         # Delete
         del occupation_clustering["Food Preparation and Serving Related Occupations"]
