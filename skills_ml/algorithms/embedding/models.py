1c7d1655273c385947aff5f8cee8bfd27f998b80
tweddielin <tweddielin@gmail.com>
2018-11-06 13:05:26 -0600

    changes based on comments

--- skills_ml/algorithms/embedding/models.py
+++ skills_ml/algorithms/embedding/models.py
@@ -140,21 +140,20 @@ class EmbeddingTransformer(BaseEstimator, TransformerMixin):
         return self.transform(X)
 
 
-def visualize_in_tensorboard(model, output_dirname=None, host="127.0.0.1"):
+def visualize_in_tensorboard(embedding_model, output_dirname=None, host="127.0.0.1"):
     if output_dirname is None:
-        output_dirname =  model.model_name.split('.')[0]
+        output_dirname =  embedding_model.model_name.split('.')[0]
 
     meta_file = f"{output_dirname}_metadata.tsv"
-    placeholder = np.zeros((len(model.wv.index2word), model.vector_size))
     output_path = os.path.join(os.getcwd(), output_dirname)
     if not os.path.isdir(output_path):
         os.mkdir(output_path)
 
     with open(os.path.join(output_path, meta_file), "wb") as file_metadata:
-        for word in model.wv.index2word:
+        for word in embedding_model.wv.index2word:
             file_metadata.write(gensim.utils.to_utf8(word) + gensim.utils.to_utf8("\n"))
 
-    embedding = tf.Variable(model.wv.vectors, trainable = False, name = f"{output_dirname}_tensor")
+    embedding = tf.Variable(embedding_model.wv.vectors, trainable = False, name = f"{output_dirname}_tensor")
     init_op = tf.global_variables_initializer()
 
     saver = tf.train.Saver()
